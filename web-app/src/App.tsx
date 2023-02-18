537f405082126e37269274e74ddc88094ece1199
Shenghan Chen <zzdjk6@gmail.com>
2023-02-19 02:09:55 +1300

    Add react-router

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 const builtAt = process.env.REACT_APP_BUILT_AT;
 
@@ -11,9 +12,11 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>This is an React web app</p>
         {builtAt && <p>Built at: {builtAt}</p>}
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <p>
+          <Link to="/about" className="App-link">
+            About
+          </Link>
+        </p>
       </header>
     </div>
   );
